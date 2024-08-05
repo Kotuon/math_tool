1cf0d94fa60c260b4d5d46da8161661f1a0b47b7
Kelson Wysocki <32180124+Kotuon@users.noreply.github.com>
2024-08-05 08:58:01 -0700

    Small changes.

--- src/MainComponent.cpp
+++ src/MainComponent.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a5fa3b079b81474b7da89bbe22acc53b5ec73d87f7964f144bccdcfec0b2e0b
-size 18260
+oid sha256:6660afc979e237fe6be96239d403c9526c86ab255305783880204baa3fb6b773
+size 18057
